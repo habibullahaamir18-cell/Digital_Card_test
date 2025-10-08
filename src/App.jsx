3eda06d5e85d40f87634b6aff5dcec131d78ff81
habibullahaamir18-cell <habibullahaamir18>
2025-10-08 02:58:08 -0700

    Added React Router for CEO, CTO, BDM manual URL navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+// App.jsx
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import OurCEO from "./components/OurCEO";
 import OurCTO from "./components/OurCTO";
 import OurBDM from "./components/OurBDM";
@@ -6,14 +8,19 @@ import OurBDM from "./components/OurBDM";
 export default function App() {
   return (
     <Router>
-      <nav style={{ textAlign: "center", margin: "20px" }}>
-        <Link to="/ceo">CEO</Link> | <Link to="/cto">CTO</Link> | <Link to="/bdm">BDM</Link>
-      </nav>
-
       <Routes>
+        {/* Redirect default path to a blank or home page */}
+        <Route path="/" element={<div style={{ textAlign: "center", marginTop: "50px" }}>
+          <h2>Welcome! Please use /ceo, /cto, or /bdm in URL to view profiles</h2>
+        </div>} />
+
+        {/* Profile routes */}
         <Route path="/ceo" element={<OurCEO />} />
         <Route path="/cto" element={<OurCTO />} />
         <Route path="/bdm" element={<OurBDM />} />
+
+        {/* Redirect any unknown path to home */}
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
   );
